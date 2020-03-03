ca3c7254a8c53490922f1d31f29394d5c0abb2f9
saini1998 <40262806+saini1998@users.noreply.github.com>
2020-03-03 15:22:09 +0000

    Update sending_text_messages.py

--- ppp6/sending_text_messages.py
+++ ppp6/sending_text_messages.py
@@ -1,10 +1,10 @@
 # SENDING TEXT MESSAGES
 # twillio api
 
-# +17209244281
+# 
 from twillio.rest import Client
-account_sid = "AC7aecc32e7572f636725904e390245431"
-auth_token = "79be12fec2409db0422c72a9b16d72f0"
+account_sid = ""
+auth_token = ""
 client = Client(account_sid, auth_token)
 
 call = client.messages.create(
